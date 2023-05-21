40281049bda8b0fac1dafc902ede52db27819cd3
dohe0342 <kimdohe1070@gmail.com>
2023-05-21 16:43:02 +0900

    from local

---
+++ watchdog_v2.py
@@ -0,0 +1,23 @@
+import os 
+import time 
+
+lastmod = []
+dirToWatchList = []
+path = './'
+for dirpath in os.walk(path):
+    #if '.git' in dirpath[0] or 'lib' in dirpath[0] or 'bin' in dirpath[0] or 'build' in dirpath[0]:
+    #    continue
+    #if 'egs2' not in dirpath[0] or 'espnet2' not in dirpath[0]:
+    #    continue
+    dirToWatch = dirpath[0]+'/'
+    lastmod.append(int(os.path.getmtime(dirToWatch)))
+    dirToWatchList.append(dirToWatch)
+#lastmod = int(os.path.getmtime(dirToWatch))
+
+while True:
+    for enum, dirToWatch in enumerate(dirToWatchList):
+        if lastmod[enum] != int(os.path.getmtime(dirToWatch)): 
+            #print('Warning: Modify Detected.') 
+            os.system('./git.sh')
+            lastmod[enum] = int(os.path.getmtime(dirToWatch)) 
+    time.sleep(1.0)
