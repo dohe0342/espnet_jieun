c8d46dedab741639fce84ab77a516bfbba9dc71a
dohe0342 <kimdohe1070@gmail.com>
2023-05-23 16:07:26 +0900

    from local

--- espnet2/lm/gpt2_lm.py
+++ espnet2/lm/gpt2_lm.py
@@ -161,10 +161,10 @@ class GPT2LM(AbsLM):
         h = self.lm(ys_new)
         h, states = h['logits'], h['past_key_values']
         h = h[:,-1]
-        print(h.size())
-        print(self.convert_matrix.size())
+        print('1', h.size())
+        print('2', self.convert_matrix.size())
         h = torch.matmul(h, self.convert_matrix)
-        print(h.size())
+        print('3', h.size())
         logp = self.log_softmax(h)
 
         # transpose state of [layer, batch] into [batch, layer]
