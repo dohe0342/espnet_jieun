91fd8e75aff66e88bc400ea6f369e2c89dea6f50
dohe0342 <kimdohe1070@gmail.com>
2023-05-30 09:14:33 +0900

    from local

--- egs2/TEMPLATE/asr1/asr.sh
+++ egs2/TEMPLATE/asr1/asr.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Set bash to 'debug' mode, it will exit on :
 # -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
-
 set -e
 set -u
 set -o pipefail
