55a123346b7c53cdd42b3f1c1284845cbe234ebd
dohe0342 <kimdohe1070@gmail.com>
2023-05-30 13:33:40 +0900

    from local

--- espnet2/bin/asr_inference_custom.py
+++ espnet2/bin/asr_inference_custom.py
@@ -972,7 +972,7 @@ def main(cmd=None):
     kwargs = vars(args)
     kwargs.pop("config", None)
     
-    kwargs['beam_size'] = 40
+    kwargs['beam_size'] = 20
     print('------------args--------------')
     print(kwargs)
     print('------------args--------------')
