7dfd3c85218887052a3321f7687d698349a99da0
dohe0342 <kimdohe1070@gmail.com>
2023-06-02 13:47:44 +0900

    from local

--- espnet2/asr/ctc.py
+++ espnet2/asr/ctc.py
@@ -116,7 +116,7 @@ class CTC(torch.nn.Module):
         Returns:
             torch.Tensor: log softmax applied 3d tensor (B, Tmax, odim)
         """
-        hs_pad /= 2.
+        hs_pad /= 5.
         return F.log_softmax(self.ctc_lo(hs_pad), dim=2)
 
     def argmax(self, hs_pad):
