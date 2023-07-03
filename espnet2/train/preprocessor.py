06e794730e5c7f4c0d05cea7c8cdee731df6bf7a
dohe0342 <kimdohe1070@gmail.com>
2023-07-03 15:31:34 +0900

    from local

--- espnet2/train/preprocessor.py
+++ espnet2/train/preprocessor.py
@@ -329,9 +329,9 @@ class CommonPreprocessor(AbsPreprocessor):
             text = data[self.text_name]
             if isinstance(text, np.ndarray):
                 return data
-            print(text)
+            print('11111', text)
             text = self.text_cleaner(text)
-            print(text)
+            print('22222', text)
             tokens = self.tokenizer.text2tokens(text)
             text_ints = self.token_id_converter.tokens2ids(tokens)
             if len(text_ints) > 150:
