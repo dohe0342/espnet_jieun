09dce4fcf3ac0c01ff970f5163d4d7e2a2bf3968
dohe0342 <kimdohe1070@gmail.com>
2023-07-03 16:38:02 +0900

    from local

--- espnet2/tasks/abs_task.py
+++ espnet2/tasks/abs_task.py
@@ -889,7 +889,7 @@ class AbsTask(ABC):
         logging.info('-'*20)
         logging.info(len(params))
         for p in params:
-            print(p)
+            print(p.size())
         logging.info('-'*20)
 
         if cls.num_optimizers != 1:
