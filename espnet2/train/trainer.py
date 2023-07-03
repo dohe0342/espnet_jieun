9a952e2db5201db25ecd0c9ed5c48ba533c508d4
dohe0342 <kimdohe1070@gmail.com>
2023-07-03 14:51:51 +0900

    from local

--- espnet2/train/trainer.py
+++ espnet2/train/trainer.py
@@ -511,6 +511,9 @@ class Trainer:
             reporter.measure_iter_time(iterator, "iter_time"), 1
         ):
             assert isinstance(batch, dict), type(batch)
+            print('-'*30)
+            print(batch)
+            print('-'*30)
 
             if distributed:
                 torch.distributed.all_reduce(iterator_stop, ReduceOp.SUM)
