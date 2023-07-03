b3020f9eab12ecf243f49fa9128e6b668a4bb16d
dohe0342 <kimdohe1070@gmail.com>
2023-07-03 15:53:42 +0900

    from local

--- egs2/librispeech_100/asr1/run_whisper_medium.sh
+++ egs2/librispeech_100/asr1/run_whisper_medium.sh
@@ -1,7 +1,7 @@
 train_set="train_clean_100"
 valid_set="dev"
-test_sets="test_clean test_other"
-#test_sets="test_clean test_other dev_clean dev_other"
+#test_sets="test_clean test_other"
+test_sets="test_clean test_other dev_clean dev_other"
 asr_tag=whisper_medium_finetune_lr1e-5_adamw_wd1e-2_3epochs
 asr_config=conf/tuning/train_asr_whisper_full.yaml
 inference_config=conf/decode_asr_whisper_noctc_greedy.yaml
@@ -13,7 +13,7 @@ inference_config=conf/decode_asr_whisper_noctc_greedy.yaml
     --lang en \
     --ngpu 1 \
     --nj 4 \
-    --stage 13 \
+    --stage 12 \
     --stop_stage 13 \
     --gpu_inference true \
     --inference_nj 1 \
