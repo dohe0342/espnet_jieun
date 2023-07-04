0a2710e6d9de72d089ecd173ba1876e0270e3859
dohe0342 <kimdohe1070@gmail.com>
2023-07-05 00:43:54 +0900

    from local

--- egs2/librispeech_100/asr1/run_whisper_small.sh
+++ egs2/librispeech_100/asr1/run_whisper_small.sh
@@ -13,6 +13,33 @@ inference_config=conf/decode_asr_whisper_noctc_greedy.yaml
     --lang en \
     --ngpu 1 \
     --nj 4 \
+    --stage 11 \
+    --stop_stage 11 \
+    --gpu_inference true \
+    --inference_nj 1 \
+    --token_type whisper_multilingual \
+    --feats_normalize '' \
+    --max_wav_duration 30 \
+    --speed_perturb_factors "0.9 1.0 1.1" \
+    --audio_format "flac.ark" \
+    --feats_type raw \
+	--cleaner whisper_en \
+    --use_lm false \
+    --asr_tag "${asr_tag}" \
+    --asr_config "${asr_config}" \
+    --inference_config "${inference_config}" \
+    --inference_asr_model valid.acc.ave.pth \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" "$@"
+
+./asr.sh \
+    --skip_data_prep true \
+    --skip_train false \
+    --skip_eval false \
+    --lang en \
+    --ngpu 1 \
+    --nj 4 \
     --stage 12 \
     --stop_stage 13 \
     --gpu_inference true \
