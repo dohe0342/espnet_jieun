5459504a34a2b1b7f771d70c09331da1e4b686be
dohe0342 <kimdohe1070@gmail.com>
2023-07-05 00:47:43 +0900

    from local

--- egs2/librispeech_100/asr1/run_whisper_small.sh
+++ egs2/librispeech_100/asr1/run_whisper_small.sh
@@ -6,31 +6,31 @@ asr_tag=whisper_small_finetune_lr1e-5_adamw_wd1e-2_3epochs_guidance_newtarget2
 asr_config=conf/tuning/train_asr_whisper_small_guidance.yaml
 inference_config=conf/decode_asr_whisper_noctc_greedy.yaml
 
-./asr.sh \
-    --skip_data_prep true \
-    --skip_train false \
-    --skip_eval false \
-    --lang en \
-    --ngpu 1 \
-    --nj 4 \
-    --stage 11 \
-    --stop_stage 11 \
-    --gpu_inference true \
-    --inference_nj 1 \
-    --token_type whisper_multilingual \
-    --feats_normalize '' \
-    --max_wav_duration 30 \
-    --speed_perturb_factors "0.9 1.0 1.1" \
-    --audio_format "flac.ark" \
-    --feats_type raw \
-    --use_lm false \
-    --asr_tag "${asr_tag}" \
-    --asr_config "${asr_config}" \
-    --inference_config "${inference_config}" \
-    --inference_asr_model valid.acc.ave.pth \
-    --train_set "${train_set}" \
-    --valid_set "${valid_set}" \
-    --test_sets "${test_sets}" "$@"
+#./asr.sh \
+#    --skip_data_prep true \
+#    --skip_train false \
+#    --skip_eval false \
+#    --lang en \
+#    --ngpu 1 \
+#    --nj 4 \
+#    --stage 11 \
+#    --stop_stage 11 \
+#    --gpu_inference true \
+#    --inference_nj 1 \
+#    --token_type whisper_multilingual \
+#    --feats_normalize '' \
+#    --max_wav_duration 30 \
+#    --speed_perturb_factors "0.9 1.0 1.1" \
+#    --audio_format "flac.ark" \
+#    --feats_type raw \
+#    --use_lm false \
+#    --asr_tag "${asr_tag}" \
+#    --asr_config "${asr_config}" \
+#    --inference_config "${inference_config}" \
+#    --inference_asr_model valid.acc.ave.pth \
+#    --train_set "${train_set}" \
+#    --valid_set "${valid_set}" \
+#    --test_sets "${test_sets}" "$@"
 
 ./asr.sh \
     --skip_data_prep true \
@@ -54,7 +54,7 @@ inference_config=conf/decode_asr_whisper_noctc_greedy.yaml
     --asr_tag "${asr_tag}" \
     --asr_config "${asr_config}" \
     --inference_config "${inference_config}" \
-    --inference_asr_model valid.acc.ave.pth \
+    --inference_asr_model 3epoch.pth \
     --train_set "${train_set}" \
     --valid_set "${valid_set}" \
     --test_sets "${test_sets}" "$@"
